97e80049551627dba0fd0cbc255b3a42f1504bdb
Everton da Rosa <everton3x@gmail.com>
2021-08-25 10:35:54 -0300

    Corrigido Builder::delTree() para não dar erro se tentar apagar um diretórios que não existe.

--- src/Build/Builder.php
+++ src/Build/Builder.php
@@ -185,6 +185,8 @@ class Builder
      */
     public static function delTree(string $dir)
     {
+        if(!file_exists($dir)) return;
+        
         $files = array_diff(scandir($dir), array('.', '..'));
         foreach ($files as $file) {
             (is_dir("$dir/$file")) ? self::delTree("$dir/$file") : unlink("$dir/$file");
